c272a9bd7268d2270194e58b38e5a38e79b2a30a
DanielAtos-20-24 <danielmoreicustodio1999@gmail.com>
2024-11-10 15:05:09 -0300

    Treinamento de códigos

---
+++ treinamento-programa-o-main/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    var quantidade = document.getElementById ('quantidade').value;
+    var de = document.getElementById ('de').value;
+    var ate = document.getElementById ('ate').value;
+
+    alert (`Quantidade de número:${quantidade}`);
+    alert (`Do número: ${de}`);
+    alert (`Até o número: ${ate}`)
+}
